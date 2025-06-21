e8010eabc14bbc70f9131e886a39c4c183d5d6cd
vmphat <vmphat.24@gmail.com>
2025-06-21 20:05:11 +0700

    remove ln in FFN

--- models/abd_transformer.py
+++ models/abd_transformer.py
@@ -155,13 +155,14 @@ class PositionWiseFeedForward(nn.Module):
         super(PositionWiseFeedForward, self).__init__()
         self.w_1 = nn.Linear(d_model, d_ff)
         self.w_2 = nn.Linear(d_ff, d_model)
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        # self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
         self.dropout_1 = nn.Dropout(dropout)
         self.relu = nn.ReLU()
         self.dropout_2 = nn.Dropout(dropout)
 
     def forward(self, x):
-        inter = self.dropout_1(self.relu(self.w_1(self.layer_norm(x))))
+        # inter = self.dropout_1(self.relu(self.w_1(self.layer_norm(x))))
+        inter = self.dropout_1(self.relu(self.w_1(x)))
         output = self.dropout_2(self.w_2(inter))
         return output
 

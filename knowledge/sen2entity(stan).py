56f60eea5e700252cd808e94d6ecdb469eb8384b
Tran Dinh Quang <quanghuyvk81@gmai.com>
2025-12-14 16:46:24 +0700

    fix: sửa lỗi logic đảo từ và ghi lặp dòng trong thuật toán OpenIE
    Chi tiết thay đổi:
    - Sửa lỗi nghiêm trọng: Đảo ngược thứ tự nối chuỗi (từ `mới + cũ` thành `cũ + mới`) khiến entity bị ngược.
    - Sửa lỗi logic vòng lặp: Di chuyển lệnh ghi file ra khỏi vòng lặp token để tránh ghi lặp lại triple nhiều lần.
    - Refactor: Sử dụng context manager (`with open`) để quản lý file an toàn hơn.
    - Cleanup: Xóa các import thừa (spacy, displacy) và hardcoded paths.

--- knowledge/sen2entity(stan).py
+++ knowledge/sen2entity(stan).py
@@ -1,98 +1,76 @@
-import spacy
-from spacy import displacy
-from stanfordcorenlp import StanfordCoreNLP
 import json
 import tqdm
+from stanfordcorenlp import StanfordCoreNLP
 
-nlp = StanfordCoreNLP(r'/kaggle/working/stanford-corenlp-4.5.4')
-sentence_path = '/kaggle/working/msvd_sentence.txt'
-entity_rel_path = '/kaggle/working/entity_total.txt'
-# doc = nlp("The 22-year-old recently won ATP Challenger tournament.")
+# Cấu hình đường dẫn (Nên đưa ra biến riêng để dễ sửa)
+CORENLP_PATH = r'/kaggle/working/stanford-corenlp-4.5.4'
+SENTENCE_PATH = '/kaggle/working/msvd_sentence.txt'
+ENTITY_REL_PATH = '/kaggle/working/entity_total.txt'
 
+# Khởi tạo NLP (Sử dụng with để đảm bảo đóng resource)
+try:
+    nlp = StanfordCoreNLP(CORENLP_PATH)
+except Exception as e:
+    print(f"Lỗi khởi tạo CoreNLP: {e}")
+    exit()
 
-f = open(sentence_path, 'r', encoding='utf-8')
-w = open(entity_rel_path, 'w', encoding='utf-8')
-lines = f.readlines()
-cnt = 0
-for line in tqdm.tqdm(lines):
-    sentence = line
-    output = nlp.annotate(sentence, properties={"annotators": "tokenize,lemma,ssplit,pos,depparse,natlog,openie",
-                                                "outputFormat": "json",
-                                                'openie.triple.strict': 'true',
-                                                'openie.max_entailments_per_clause': '1'
-                                                })
-    data = json.loads(output)
-    # result = data['sentences'][0]['openie']
-    # print(result)
-    # print(data['sentences'][0].keys())
-    # print(data['sentences'][0]['openie'])
-    # print(data['sentences'][0]['tokens'])
+print("Bắt đầu xử lý...")
 
-    for i in range(len(data['sentences'])):
-        result = data["sentences"][i]["openie"]  # Trực tiếp lấy openie của sentence i
-        lemmas = data["sentences"][i]["tokens"]  # Trực tiếp lấy tokens của sentence i
-        cnt += 1
-        
-        for rel in result:
-            l_relation, l_object, l_subject = '', '', ''
-            span = str(rel['subjectSpan']), str(rel['objectSpan']), str(rel['relationSpan'])
+# Dùng 'with' để quản lý file an toàn
+with open(SENTENCE_PATH, 'r', encoding='utf-8') as f, \
+     open(ENTITY_REL_PATH, 'w', encoding='utf-8') as w:
+    
+    lines = f.readlines()
+    cnt = 0
+    
+    for line in tqdm.tqdm(lines):
+        sentence = line.strip()
+        if not sentence: continue
+
+        try:
+            output = nlp.annotate(sentence, properties={
+                "annotators": "tokenize,lemma,ssplit,pos,depparse,natlog,openie",
+                "outputFormat": "json",
+                'openie.triple.strict': 'true',
+                'openie.max_entailments_per_clause': '1'
+            })
+            data = json.loads(output)
+        except Exception as e:
+            print(f"Lỗi xử lý câu: {sentence[:20]}... - {e}")
+            continue
+
+        for s_idx, sent_data in enumerate(data['sentences']):
+            result = sent_data["openie"]
+            tokens = sent_data["tokens"]
             
-            # Lấy lemma của subject
-            l_subject1 = lemmas[rel['subjectSpan'][0]:rel['subjectSpan'][1]]
-            for h in range(len(l_subject1)):
-                l_subject = l_subject1[h]['lemma'] + ' ' + l_subject
-            
-            # Lấy lemma của object
-            l_object1 = lemmas[rel['objectSpan'][0]:rel['objectSpan'][1]]
-            for s in range(len(l_object1)):
-                l_object = l_object1[s]['lemma'] + ' ' + l_object
-            
-            # Lấy lemma của relation
-            l_relation1 = lemmas[rel['relationSpan'][0]:rel['relationSpan'][1]]
-            for j in range(len(l_relation1)):
-                l_relation = l_relation1[j]['lemma'] + ' ' + l_relation
-                # l_relation = lemmas[i][rel['relationSpan'][0]:rel['relationSpan'][1]][0][
-                #              0:rel['relationSpan'][1] - rel['relationSpan'][0]]['lemma']
-                # relationSent1 = rel['subject'], rel['object'], rel['relation']
-                relationSent = l_subject, '&', l_object, '&', l_relation
-                # print(relationSent)
-                w.writelines(relationSent)
-                w.writelines('\n')
+            for rel in result:
+                # Hàm helper để ghép chuỗi từ tokens dựa vào span
+                def get_span_text(span_indices, tokens_list):
+                    # span_indices là [start, end)
+                    words = []
+                    for i in range(span_indices[0], span_indices[1]):
+                        # Chọn 'lemma' hoặc 'originalText' tùy nhu cầu
+                        words.append(tokens_list[i]['lemma']) 
+                    return " ".join(words)
 
-                # print(str(cnt))
-                # print(relationSent1)
-print('total number is ' + str(cnt) + '\n')
-print('reslut write to ' + entity_rel_path)
-w.close()
-f.close()
+                try:
+                    # FIX LỖI 1: Dùng hàm join để nối từ đúng thứ tự
+                    str_subject = get_span_text(rel['subjectSpan'], tokens)
+                    str_object = get_span_text(rel['objectSpan'], tokens)
+                    str_relation = get_span_text(rel['relationSpan'], tokens)
 
-# sentence = 'Guangdong University of Foreign Studies is located in Guangzhou.'
-# print('Tokenize:', nlp.word_tokenize(sentence))
-# print('Part of Speech:', nlp.pos_tag(sentence))
-# print('Named Entities:', nlp.ner(sentence))
-# print('Constituency Parsing:', nlp.parse(sentence))  # 语法树
-# print('Dependency Parsing:', nlp.dependency_parse(sentence))  # 依存句法
-# nlp.close()  # Do not forget to close! The backend server will consume a lot memery
+                    # Định dạng format output
+                    # Lưu ý: Thêm ký tự xuống dòng rõ ràng
+                    relationSent = f"{str_subject} & {str_object} & {str_relation}\n"
+                    
+                    # FIX LỖI 2: Ghi file NGOÀI vòng lặp xử lý từ
+                    w.write(relationSent)
+                    cnt += 1
+                except IndexError:
+                    continue
 
-# lines = f.readlines()
-# for line in lines:
-#     # print(line)
-#     # print(type(line))
-#     doc = nlp(line)
-# displacy.render(doc, style='dep', jupyter=False)  # need to run in Jupyter
+print(f'Tổng số triple trích xuất: {cnt}')
+print(f'Kết quả đã ghi vào: {ENTITY_REL_PATH}')
 
-# break
-# for tok in doc:
-#     print(tok.text + "---------------->" + tok.pos_ + ' ' + tok.dep_ + ' ' + tok.tag_)
-#     if tok.pos_ == 'NOUN':
-#         w.writelines(tok.text + ' ')
-#     # if tok.dep_ == 'pobj' or 'dobj':
-#     #     w.writelines(tok.text + ' ')
-#     if tok.pos_ == 'VERB':
-#         w.writelines(tok.lemma_ + ' ')
-#     if tok.pos_ == 'SPACE':
-#         w.write('\n')
-# print(tok.text + '------------>' + tok.dep_)
-
-# for tok in doc:
-#     print(tok.text, "...", tok.dep_)
+# Đóng connection tới server Java
+nlp.close()

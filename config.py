791795c5c4d1136f876ceb9ad4a5a5a9ac33d1f3
vmphat <vmphat.24@gmail.com>
2026-07-18 06:33:53 +0700

    siglip2pooled-t5base-msvd

--- config.py
+++ config.py
@@ -145,8 +145,8 @@ class TransformerConfig:
     d_model càng lớn -> projection từ feature (SigLIP2 1536-d) càng ít mất mát.
     """;
     # t5_model_name = "google/flan-t5-small"  #  80M params | d_model 512
-    # t5_model_name = "google/flan-t5-base"   # 250M params | d_model 768
-    t5_model_name = "google/flan-t5-large"  # 780M params | d_model 1024
+    t5_model_name = "google/flan-t5-base"   # 250M params | d_model 768
+    # t5_model_name = "google/flan-t5-large"  # 780M params | d_model 1024
     # t5_model_name = "google/flan-t5-xl"     #   3B params | d_model 2048
 
     dropout = 0.1
@@ -156,7 +156,7 @@ class TransformerConfig:
     # Chọn N block cách đều (linspace), luôn gồm block 0 (mang relative attention bias):
     #   small/base: 8/12 block  | large/xl/xxl: 24 block
     #   large giữ 6 -> block [0, 5, 9, 14, 18, 23]
-    num_decoder_layers = 4
+    num_decoder_layers = 0
 
 
 class SCSTConfig:

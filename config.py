5dd3b0aac344e30a5886a708c75d4e94fd6cf5a9
vmphat <vmphat.24@gmail.com>
2026-07-18 07:58:59 +0700

    siglip2pooled-t5small-msvd

--- config.py
+++ config.py
@@ -144,8 +144,8 @@ class TransformerConfig:
       xxl    ~11B  | d_model 4096 | 24 layer | 64 head | d_ff 10240 (gated)
     d_model càng lớn -> projection từ feature (SigLIP2 1536-d) càng ít mất mát.
     """;
-    # t5_model_name = "google/flan-t5-small"  #  80M params | d_model 512
-    t5_model_name = "google/flan-t5-base"   # 250M params | d_model 768
+    t5_model_name = "google/flan-t5-small"  #  80M params | d_model 512
+    # t5_model_name = "google/flan-t5-base"   # 250M params | d_model 768
     # t5_model_name = "google/flan-t5-large"  # 780M params | d_model 1024
     # t5_model_name = "google/flan-t5-xl"     #   3B params | d_model 2048
 

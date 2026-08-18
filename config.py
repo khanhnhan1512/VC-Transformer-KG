6bd67c70a13ae1c924126607b11d7d68b07408cb
vmphat <vmphat.24@gmail.com>
2026-08-19 03:53:10 +0700

    msrvtt-3feats-uniDec-3layer

--- config.py
+++ config.py
@@ -187,8 +187,8 @@ class SCSTConfig:
 
 class TrainConfig:
     # corpus = "MSVD"
-    # corpus = "MSRVTT"
-    corpus = "VATEX"
+    corpus = "MSRVTT"
+    # corpus = "VATEX"
     if   corpus == "MSVD"  : loader = MSVDLoaderConfig
     elif corpus == "MSRVTT": loader = MSRVTTLoaderConfig
     elif corpus == "VATEX" : loader = VATEXLoaderConfig
